edcd5d592205326b72615514b51e47c58015650f
surajverma2587 <surajverma2587@gmail.com>
2023-06-01 20:22:56 +0100

    add state to counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,16 +3,46 @@ import { Component } from "react";
 export class Counter extends Component {
   constructor(props) {
     super(props);
+
+    this.state = { count: 0 };
   }
+
+  handleDecrement = () => {
+    if (this.state.count > 0) {
+      this.setState({
+        count: this.state.count - 1,
+      });
+    }
+  };
+
+  handleIncrement = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      count: 0,
+    });
+  };
 
   render() {
     return (
       <div className="border m-3 p-4 rounded">
-        <div className="sub-title text-center mb-3">Counter: 0</div>
+        <div className="sub-title text-center mb-3">
+          Counter: {this.state.count}
+        </div>
         <div className="d-grid gap-2 col-6 mx-auto">
-          <button className="btn btn-success">Decrement</button>
-          <button className="btn btn-danger">Reset</button>
-          <button className="btn btn-success">Increment</button>
+          <button className="btn btn-success" onClick={this.handleDecrement}>
+            Decrement
+          </button>
+          <button className="btn btn-danger" onClick={this.handleReset}>
+            Reset
+          </button>
+          <button className="btn btn-success" onClick={this.handleIncrement}>
+            Increment
+          </button>
         </div>
       </div>
     );
