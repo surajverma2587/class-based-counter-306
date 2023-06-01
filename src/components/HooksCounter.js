4aedfa3b48fa9f0d47494bc500fb576082d068b5
surajverma2587 <surajverma2587@gmail.com>
2023-06-01 20:38:59 +0100

    add hooks

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -1,11 +1,35 @@
+import { useState } from "react";
+
 export const HooksCounter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
     <div className="border m-3 p-4 rounded">
-      <div className="sub-title text-center mb-3">Hooks Counter: 0</div>
+      <div className="sub-title text-center mb-3">Hooks Counter: {count}</div>
       <div className="d-grid gap-2 col-6 mx-auto">
-        <button className="btn btn-success">Decrement</button>
-        <button className="btn btn-danger">Reset</button>
-        <button className="btn btn-success">Increment</button>
+        <button className="btn btn-success" onClick={handleDecrement}>
+          Decrement
+        </button>
+        <button className="btn btn-danger" onClick={handleReset}>
+          Reset
+        </button>
+        <button className="btn btn-success" onClick={handleIncrement}>
+          Increment
+        </button>
       </div>
     </div>
   );
