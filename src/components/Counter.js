491424b297654aa635a02273d8797ce03f580e9c
surajverma2587 <surajverma2587@gmail.com>
2023-06-01 20:13:23 +0100

    complete styled counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,6 +6,15 @@ export class Counter extends Component {
   }
 
   render() {
-    return <div>Counter</div>;
+    return (
+      <div className="border m-3 p-4 rounded">
+        <div className="sub-title text-center mb-3">Counter: 0</div>
+        <div className="d-grid gap-2 col-6 mx-auto">
+          <button className="btn btn-success">Decrement</button>
+          <button className="btn btn-danger">Reset</button>
+          <button className="btn btn-success">Increment</button>
+        </div>
+      </div>
+    );
   }
 }
