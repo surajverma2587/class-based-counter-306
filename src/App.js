bc8b5831756da08430fc6dee7064018cfb915244
surajverma2587 <surajverma2587@gmail.com>
2023-06-01 19:57:21 +0100

    add components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { Banner } from "./components/Banner";
+import { Counter } from "./components/Counter";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Banner />
+      <Counter />
+    </div>
+  );
 };
